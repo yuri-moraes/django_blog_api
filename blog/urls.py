bc66d976ea29997e71db0e31383a0cfe19b0ec40
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-10-18 14:52:44 -0300

    feat: post_detail page created

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('posts/<int:post_id>/post_detail', views.get_post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
-posts = Post.objects.all()
-context = {
-    'posts': posts
-}
-
 def home(req):
-    return render(req, 'blog/home.html', context)
+    posts = Post.objects.all()
+    context = { 'posts': posts }
+    return render(req, 'blog/home.html', context) 
+
+def get_post_detail(req, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = { 'post': post }
+    return render(req, 'blog/post_detail.html',context)
