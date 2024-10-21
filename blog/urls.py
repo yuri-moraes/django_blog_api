20a8c48a2de8f036634e13f06d21e400dc6a1ca0
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-10-21 10:16:43 -0300

    feat: adding first static file

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+from core import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/<int:post_id>/post_detail', views.get_post_detail, name='post_detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
